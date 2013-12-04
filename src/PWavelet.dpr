7d06aed9533355ec7e0506fbbcb348a42f0d00f4
Alexey <gorkoff@gmail.com>
2013-12-04 12:50:12 +0400

    Initial commt

---
+++ src/PWavelet.dpr
@@ -0,0 +1,14 @@
+program PWavelet;
+
+uses
+  Vcl.Forms,
+  UFMain in 'UFMain.pas' {FMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFMain, FMain);
+  Application.Run;
+end.
